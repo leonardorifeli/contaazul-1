5461bd47f60556d24e72c3545100adc646c0f426
orlandocorreia2 <orlandocorreia2@hotmail.com>
2017-02-16 10:55:58 -0200

    Update permissions.php

--- models/permissions.php
+++ models/permissions.php
@@ -4,16 +4,23 @@ class permissions extends model{
         parent::__construct();
     }
     public function getPermissions($group, $id_company){
-        $params = "";
+        $array = array();
         $sql = "SELECT params FROM permission_groups WHERE id = '$group' AND id_company = '$id_company'";
         $sql = $this->db->query($sql);
         if ($sql->rowCount() > 0) {
-            $sql = $sql->fetch();
-            if(!empty($sql['params'])){
-                $params = $sql['params'];
+            $row = $sql->fetch();
+            if(!empty($row['params'])){
+                $sql = "SELECT name FROM permission_params WHERE id IN(".$row['params'].") AND id_company = '$id_company'";
+                $sql = $this->db->query($sql);
+                if($sql->rowCount() > 0){
+                    $sql = $sql->fetchAll();
+                    foreach($sql as $param){
+                       $array[] = $param['name']; 
+                    } 
+                }
             }       
         }
-        return $params;
+        return $array;
     }
 }
 
