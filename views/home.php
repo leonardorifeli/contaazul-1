3a75c9d62175590f22bd9341256d811afe4ea3ef
orlandocorreia2 <orlandocorreia2@hotmail.com>
2017-03-28 08:43:40 -0300

    DashBoard

--- views/home.php
+++ views/home.php
@@ -1,1 +1,50 @@
-<h1><?php echo $company['name']; ?></h1>
+<div class="db-row1">
+    <div class="db-grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99.99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+    <div class="db-grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99.99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+    <div class="db-grid-1">
+        <div class="db-grid-area">
+            <div class="db-grid-area-count">99.99</div>
+            <div class="db-grid-area-legend">Legenda</div>
+        </div>
+    </div>
+</div>
+
+<div class="db-row2">
+    <div class="db-grid-2">
+        <div class="db-info">
+            <div class="db-info-title">Título</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+    <div class="db-grid-1">
+        <div class="db-info">
+            <div class="db-info-title">Título</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+</div>
+
+<div class="db-row2">
+    <div class="db-grid-2">
+        <div class="db-info">
+            <div class="db-info-title">Título</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+    <div class="db-grid-1">
+        <div class="db-info">
+            <div class="db-info-title">Título</div>
+            <div class="db-info-body">...</div>
+        </div>
+    </div>
+</div>
